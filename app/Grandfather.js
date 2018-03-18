2cc441b7d6551238c7bdbad626cfd5acf64ac874
zyw327 <327523057@qq.com>
2018-03-18 16:29:58 +0800

    repair package-lock.json

--- app/Grandfather.js
+++ app/Grandfather.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Father from './Father';
 import PropsTypes  from 'prop-types';
+import './style.less';
 
 //context上下文数据是单向流动，只能由父级传给子级，父级可以通过传函数的方式进行双向绑定
 
@@ -8,8 +9,13 @@ class Grandfather extends Component {
     constructor() {
         super();
         this.state = {
-            a: 1000
+            a: 1000,
+            r: 0,
+            g: 0,
+            b: 0,
+            // changeR: (this.changeR).bind(this)
         };
+        this.changeR = (this.changeR).bind(this);
     }
 
     // 得到孩子上下文,实际上这里表示一种设置，返回一个对象，这个对象就是现在这个家族体系共享的上下文。将上下文中的a值变为自己状态中的a值
@@ -20,6 +26,10 @@ class Grandfather extends Component {
         }
     }
     
+    changeR(event) {
+        this.setState({r: event.target.value});
+    }
+
     setA() {
         this.setState({a: this.state.a - 1});
     }
@@ -30,6 +40,8 @@ class Grandfather extends Component {
                 <h1>爷爷{this.state.a}</h1>
                 <Father a={this.state.a}></Father>
                 <button onClick={() => {this.setState({a: this.state.a - 1})}}>爷爷改a</button>
+                <input className='barName' type='range' min='0' max='255' value={this.state.r} onChange={this.changeR} />
+                <input type='number' min='0' max='255' value={this.state.r} onChange={this.changeR} />
             </div>
         );
     }
--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,16 @@ module.exports = {
                   presets: ["es2015", 'react']
               }
           }
+      },
+      {
+        test: /\.less$/,
+        use: [{
+          loader: 'style-loader'
+        },{
+          loader: 'css-loader'
+        },{
+          loader: 'less-loader'
+        }]
       }
     ]
   },
