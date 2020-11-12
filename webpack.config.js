2616e6b487816037b41d7a9230b4869f2c58f9b4
zyw327 <327523057@qq.com>
2020-11-12 18:07:22 +0800

    优化运行及升级依赖

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: __dirname + "/app/main.js", // string | object | array
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"), // string
+    filename: "bundle.js", // string
+  },
+  module: {
+    // configuration regarding modules
+    rules: [
+      // rules for modules (configure loaders, parser options, etc.)
+      {
+          test: /\.js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+              loader: 'babel-loader',
+              options: {
+                  presets: ["es2015", 'react']
+              }
+          }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': '"production"',
+    }),
+  ],
+  watch: true
+}
